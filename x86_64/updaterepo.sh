3094b9aba115d8665e5ea300cd7850a8c7ddd6a7
spookykidmm <spookykidmm@gmail.com>
2019-10-01 16:06:58 -0400

    Tried to fix .files

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -3,8 +3,9 @@
 rm spooky_aur.*
 repo-add spooky_aur.db.tar.gz *.pkg.tar.xz
 sleep 2
-rm spooky_aur.db
+rm spooky_aur.db spooky_aur.files
 sleep 2
 cp spooky_aur.db.tar.gz spooky_aur.db
+sleep 2
 cp spooky_aur.files.tar.gz spooky_aur.files
 echo "Repo Updated!!"
